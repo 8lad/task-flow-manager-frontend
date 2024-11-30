6aefb51b3f0a7a6aa6d0a8c252ba38738f6dddfa
Vlad <vlasisla1988@gmail.com>
2024-11-30 18:19:58 +0200

    feat: Add decorative circles on the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,16 @@ import { Heading } from '@/components/shared/Heading/Heading';
 import { MainAnimation } from '@/components/shared/MainAnimation/MainAnimation';
 
 export default function Home() {
+  const renderDecorativeCircles = () => (
+    <>
+      <div className="absolute -bottom-10 -left-40 z-[-1] h-96 w-96 rounded-full bg-mainAccent opacity-15 blur-2xl"></div>
+      <div className="-tor-10 absolute -right-40 z-[-1] h-96 w-96 rounded-full bg-mainAccent opacity-15 blur-2xl"></div>
+    </>
+  );
+
   return (
-    <div className="flex min-h-screen flex-col">
+    <div className="relative flex min-h-screen flex-col overflow-hidden">
+      {renderDecorativeCircles()}
       <Header className="mb-12" />
       <main className="grow">
         <Container>
