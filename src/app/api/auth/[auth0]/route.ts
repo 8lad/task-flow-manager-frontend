c453c943d979e83e37465e8cf68f1a751a47a990
Vlad <vlasisla1988@gmail.com>
2024-12-03 13:26:43 +0200

    feat: Update login flow. Fix error when a user decline auth flow

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -1,11 +1,34 @@
 import { MainRoutes } from '@/utils/routes';
-import { handleAuth, handleLogin, handleLogout } from '@auth0/nextjs-auth0';
+import { handleAuth, handleCallback, handleLogin, handleLogout } from '@auth0/nextjs-auth0';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+const ERROR_SEARCH_PARAM = 'error';
+const USER_DECLINE_ERROR = 'access_denied';
 
 export const GET = handleAuth({
-  login: handleLogin({
-    returnTo: MainRoutes.Dashboard,
-  }),
+  login: handleLogin({ returnTo: MainRoutes.Home }),
   logout: handleLogout({
     returnTo: MainRoutes.Home,
   }),
+  callback: (req: NextApiRequest, res: NextApiResponse) => {
+    try {
+      if (req.url) {
+        const url = new URL(req.url);
+        const error = url.searchParams.get(ERROR_SEARCH_PARAM);
+        if (error === USER_DECLINE_ERROR) {
+          return new Response(null, {
+            status: 302,
+            headers: { Location: MainRoutes.Home },
+          });
+        }
+      }
+      return handleCallback(req, res);
+    } catch (error) {
+      console.error(error);
+      new Response(null, {
+        status: 302,
+        headers: { Location: MainRoutes.Home },
+      });
+    }
+  },
 });
