4a285c36798e9e8962ca3312862bd0ba89eba3b4
Vlad <vlasisla1988@gmail.com>
2024-12-04 00:20:30 +0200

    feat: Add protection for all routes except the main page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,9 @@
 import { Heading } from '@/components/shared/Heading/Heading';
-import { MainRoutes } from '@/utils/routes';
-import { withPageAuthRequired } from '@auth0/nextjs-auth0';
 
-async function Page() {
+export default function Page() {
   return (
     <div>
       <Heading level="h1">Dashboard page</Heading>
     </div>
   );
 }
-
-export default withPageAuthRequired(Page, { returnTo: MainRoutes.Dashboard });
