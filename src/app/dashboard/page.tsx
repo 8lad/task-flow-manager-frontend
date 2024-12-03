6cae57d07e0359e87067e6edb0ec2ebc3318a109
Vlad <vlasisla1988@gmail.com>
2024-12-03 19:41:21 +0200

    feat: Add protected dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,13 @@
 import { Heading } from '@/components/shared/Heading/Heading';
+import { MainRoutes } from '@/utils/routes';
+import { withPageAuthRequired } from '@auth0/nextjs-auth0';
 
-export default function Page() {
+async function Page() {
   return (
     <div>
       <Heading level="h1">Dashboard page</Heading>
     </div>
   );
 }
+
+export default withPageAuthRequired(Page, { returnTo: MainRoutes.Dashboard });
